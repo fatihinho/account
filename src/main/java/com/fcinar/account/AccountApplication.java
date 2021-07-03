fe444b09e000d1f73256d07beaa7a8e72fd30832
Fatih Çınar <fcinar_38@hotmail.com>
2021-07-03 23:47:04 +0300

    Creating customer while app starting

--- src/main/java/com/fcinar/account/AccountApplication.java
+++ src/main/java/com/fcinar/account/AccountApplication.java
@@ -1,13 +1,29 @@
 package com.fcinar.account;
 
+import com.fcinar.account.model.Customer;
+import com.fcinar.account.repository.ICustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class AccountApplication {
+public class AccountApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AccountApplication.class, args);
-	}
+    private final ICustomerRepository customerRepository;
 
+    public AccountApplication(ICustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountApplication.class, args);
+    }
+
+    @Bean
+    @Override
+    public void run(String... args) throws Exception {
+        Customer customer = customerRepository.save(new Customer("Fatih", "Çınar"));
+        System.out.println(customer);
+    }
 }
