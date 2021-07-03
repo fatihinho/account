992bc1996588ec401f82fab4f265da6fa00f4e16
Fatih Çınar <fcinar_38@hotmail.com>
2021-07-03 23:50:50 +0300

    Fixed variables type

--- src/main/java/com/fcinar/account/dto/CustomerAccountDto.kt
+++ src/main/java/com/fcinar/account/dto/CustomerAccountDto.kt
@@ -5,7 +5,7 @@ import java.time.LocalDateTime
 
 data class CustomerAccountDto(
     val id: String,
-    var balance: BigDecimal? = BigDecimal.ZERO,
+    val balance: BigDecimal? = BigDecimal.ZERO,
     val transactions: Set<TransactionDto>?,
     val creationDate: LocalDateTime
 )
