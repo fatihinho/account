158dfcbe8fbfa201a9702860827fd5c7aa413b14
Fatih Çınar <fcinar_38@hotmail.com>
2021-07-03 23:54:33 +0300

    Added accountDtoConverter property

--- src/main/java/com/fcinar/account/dto/converter/TransactionDtoConverter.java
+++ src/main/java/com/fcinar/account/dto/converter/TransactionDtoConverter.java
@@ -3,19 +3,15 @@ package com.fcinar.account.dto.converter;
 import com.fcinar.account.dto.TransactionDto;
 import com.fcinar.account.model.Transaction;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 
+@Component
 public class TransactionDtoConverter {
-    public final AccountDtoConverter accountDtoConverter;
-
-    public TransactionDtoConverter(AccountDtoConverter accountDtoConverter) {
-        this.accountDtoConverter = accountDtoConverter;
-    }
 
     public TransactionDto convert(@NotNull Transaction from) {
         return new TransactionDto(from.getId(),
                 from.getTransactionType(),
                 from.getAmount(),
-                from.getTransactionDate(),
-                accountDtoConverter.convert(from.getAccount()));
+                from.getTransactionDate());
     }
 }
