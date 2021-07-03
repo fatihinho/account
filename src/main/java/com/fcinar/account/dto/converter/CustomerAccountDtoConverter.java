13bcbe800b28814258926cdf75499122f7bf8bc8
Fatih Çınar <fcinar_38@hotmail.com>
2021-07-03 23:52:52 +0300

    Fixed property accessibility

--- src/main/java/com/fcinar/account/dto/converter/CustomerAccountDtoConverter.java
+++ src/main/java/com/fcinar/account/dto/converter/CustomerAccountDtoConverter.java
@@ -3,12 +3,14 @@ package com.fcinar.account.dto.converter;
 import com.fcinar.account.dto.CustomerAccountDto;
 import com.fcinar.account.model.Account;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Component
 public class CustomerAccountDtoConverter {
-    public final TransactionDtoConverter transactionDtoConverter;
+    private final TransactionDtoConverter transactionDtoConverter;
 
     public CustomerAccountDtoConverter(TransactionDtoConverter transactionDtoConverter) {
         this.transactionDtoConverter = transactionDtoConverter;
@@ -17,7 +19,10 @@ public class CustomerAccountDtoConverter {
     public CustomerAccountDto convert(@NotNull Account from) {
         return new CustomerAccountDto(Objects.requireNonNull(from.getId()),
                 from.getBalance(),
-                Objects.requireNonNull(from.getTransactions()).stream().map(transactionDtoConverter::convert).collect(Collectors.toSet()),
+                Objects.requireNonNull(from.getTransactions())
+                        .stream()
+                        .map(transactionDtoConverter::convert)
+                        .collect(Collectors.toSet()),
                 from.getCreationDate());
     }
 }
--- src/main/java/com/fcinar/account/dto/converter/CustomerDtoConverter.java
+++ src/main/java/com/fcinar/account/dto/converter/CustomerDtoConverter.java
@@ -1,21 +1,31 @@
 package com.fcinar.account.dto.converter;
 
+import com.fcinar.account.dto.AccountCustomerDto;
 import com.fcinar.account.dto.CustomerAccountDto;
 import com.fcinar.account.dto.CustomerDto;
 import com.fcinar.account.model.Customer;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.stereotype.Component;
 
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+@Component
 public class CustomerDtoConverter {
-    public final CustomerAccountDtoConverter customerAccountDtoConverter;
+    private final CustomerAccountDtoConverter customerAccountDtoConverter;
 
     public CustomerDtoConverter(CustomerAccountDtoConverter converter) {
         this.customerAccountDtoConverter = converter;
     }
 
     public CustomerDto convert(@NotNull Customer from) {
-        return new CustomerDto(from.getId(), from.getName(), from.getSurname(), Objects.requireNonNull(from.getAccounts()).stream().map(customerAccountDtoConverter::convert).collect(Collectors.toSet()));
+        return new CustomerDto(from.getId(), from.getName(), from.getSurname(), Objects.requireNonNull(from.getAccounts())
+                .stream()
+                .map(customerAccountDtoConverter::convert)
+                .collect(Collectors.toSet()));
+    }
+
+    public AccountCustomerDto convertToAccountCustomerDto(@NotNull Customer from) {
+        return new AccountCustomerDto(from.getId(), from.getName(), from.getSurname());
     }
 }
