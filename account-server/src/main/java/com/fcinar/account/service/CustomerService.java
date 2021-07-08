6910fb4735faf4dea93f18bf42d5f88fc8fb55fa
Fatih Çınar <fcinar_38@hotmail.com>
2021-07-09 00:47:58 +0300

    Added getALlCustomer method

--- account-server/src/main/java/com/fcinar/account/service/CustomerService.java
+++ account-server/src/main/java/com/fcinar/account/service/CustomerService.java
@@ -7,6 +7,9 @@ import com.fcinar.account.model.Customer;
 import com.fcinar.account.repository.ICustomerRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class CustomerService {
     private final ICustomerRepository customerRepository;
@@ -24,5 +27,10 @@ public class CustomerService {
 
     public CustomerDto getCustomerById(String customerId) {
         return customerDtoConverter.convert(findCustomerById(customerId));
+    }
+
+    public List<CustomerDto> getAllCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+        return customers.stream().map(customerDtoConverter::convert).collect(Collectors.toList());
     }
 }
